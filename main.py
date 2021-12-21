27746b180a8ca5912af4932f3843c943f73b51c8
kaloian04 <kaloian.koyuv@gmail.com>
2021-12-21 21:04:51 +0200

    changing the font for the Text widget

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 
 root = Tk()
 root.geometry("1024x768")
+root.title("Zennote")
 
 frame = Frame(root)
 frame.pack()
@@ -18,7 +19,7 @@ menubar = Menu(frame)
 menubar.add_command(label = "Save", command=save)
 menubar.add_command(label = "Exit", command=exit)
 
-editor = Text(frame, width=1000, height=1000)
+editor = Text(frame, width=1000, height=1000, font = ("monospace", 13))
 
 with open('notes.plain') as f:
     lines = f.read()
