33317c0c7b7569b3024c05e2fc8c11d2ea74f18b
kaloian04 <kaloian.koyuv@gmail.com>
2021-12-21 20:36:01 +0200

    fixes

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 from tkinter import *
-import os
-import subprocess
-
-FILEBROWSER_PATH = os.path.join(os.getenv('WINDIR'), 'explorer.exe')
 
 root = Tk()
 root.geometry("1024x768")
@@ -10,29 +6,26 @@ root.geometry("1024x768")
 frame = Frame(root)
 frame.pack()
 
-#def new_file():
-
-
-#def open_file(path):
-
-
 def save():
-    file = open("test.txt", "w")
-    file.write(entry.get())
+    file = open("notes.plain", "w")
+    file.write(editor.get("1.0",'end-1c'))
     file.close()
 
 def exit():
     root.destroy()
 
 menubar = Menu(frame)
-#menubar.add_command(label = "New File", command=new_file)
-#menubar.add_command(label = "Open File", command=open_file)
 menubar.add_command(label = "Save", command=save)
-#enubar.add_command(label = "Save As", command=save_as)
 menubar.add_command(label = "Exit", command=exit)
 
-entry = Entry(frame, width=100000)
-entry.pack()
+editor = Text(frame, width=1000, height=1000)
+
+with open('notes.plain') as f:
+    lines = f.read()
+
+editor.insert("1.0", lines)
+
+editor.pack()
 
 root.config(menu = menubar)
 
